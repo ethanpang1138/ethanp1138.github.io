788ab666e87a475e1ff49c4d784c7dac6fbdca9e
Ethan Pang <ethanp5@berkeley.edu>
2021-05-19 22:33:45 -0700

    initial

---
+++ ethanpang5.github.io/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './css/custom.css';
+import './css/styles.css';
+import ExperienceModal from "./components/ExperienceModal"
+import Education from "./components/Education"
+
+function App() {
+  return (
+    <div className="App">
+      <Education/>
+    </div>
+  );
+}
+
+export default App;
